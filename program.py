9ecf6b90e67492a8f4ae159bdc22a3027e68c1b9
Jappie Klooster <jappieklooster@hotmail.com>
2016-03-20 22:48:36 +0100

    From input to processing

--- program.py
+++ program.py
@@ -1,26 +1,48 @@
 #!/usr/bin/python
 import re
 import os
-
+from subprocess import check_output, CalledProcessError, TimeoutExpired
 
 
 symbolTable = {}
 newFile = []
-for line in open('offer.tex', 'r'):
-    match = re.findall('((?<=\$)[A-Z]+)+', line)
-    for var in match:
-        if var not in symbolTable:
-            symbolTable[var] = input("please specify %s \n" % var)
-        value = symbolTable[var]
-        line = line.replace('\\$'+var,value)
+with open('offer.tex', 'r') as templateFile:
+    for line in templateFile:
+        match = re.findall('((?<=\$)[A-Z]+)+', line)
+        for var in match:
+            if var not in symbolTable:
+                symbolTable[var] = input("please specify %s \n" % var)
+            value = symbolTable[var]
+            line = line.replace('\\$'+var,value)
 
-    newFile.append(line)
+        newFile.append(line)
 
 name = "NAME"
 outPath = "out"
 if not os.path.exists(outPath):
     os.makedirs(outPath)
 
-outputFile = open('{}/{}.tex'.format(outPath, symbolTable[name]), 'w')
-for line in newFile:
-    outputFile.write(line)
+outFileName = '{}.tex'.format(symbolTable[name])
+outFile = '{}/{}'.format(outPath, outFileName)
+with open(outFile, 'w') as outputFile:
+    for line in newFile:
+        outputFile.write(line)
+
+def executeAction(command):
+    print("executing %s" % ' '.join(command))
+    result = "" 
+    try:
+        result = check_output(command, timeout=5, cwd=outPath)
+    except CalledProcessError as e: 
+        result = e.output
+    except TimeoutExpired as e: 
+        result = e.output
+    print(result.decode("utf8"))
+
+processPdf = [
+    "pdflatex",
+    "-halt-on-error",
+    "-shell-escape",
+    outFileName
+]
+executeAction(processPdf)
