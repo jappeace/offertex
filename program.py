6359b9beddf40ebb31c7a092fa1202e353e47933
Jappie Klooster <jappieklooster@hotmail.com>
2016-03-20 22:27:24 +0100

    writing the compiled latex script

--- program.py
+++ program.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 import re
+import os
 
-fli = open('offer.tex', 'r')
+
 
 symbolTable = {}
 newFile = []
-for line in fli:
+for line in open('offer.tex', 'r'):
     match = re.findall('((?<=\$)[A-Z]+)+', line)
     for var in match:
         if var not in symbolTable:
@@ -15,5 +16,11 @@ for line in fli:
 
     newFile.append(line)
 
+name = "NAME"
+outPath = "out"
+if not os.path.exists(outPath):
+    os.makedirs(outPath)
+
+outputFile = open('{}/{}.tex'.format(outPath, symbolTable[name]), 'w')
 for line in newFile:
-    print(line)
+    outputFile.write(line)
